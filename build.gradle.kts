134837882affda3d7558e8926f6e697b81504d20
Subin Lee <chandar.sblee@gmail.com>
2021-03-19 10:00:18 +0900

    Add axios, ant design dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,13 +9,19 @@ repositories {
     jcenter()
     mavenCentral()
     maven { url = uri("https://maven.pkg.jetbrains.space/public/p/kotlin/p/kotlin/kotlin-js-wrappers") }
+    maven("https://dl.bintray.com/samgarasx/kotlin-js-wrappers")
 }
 
 dependencies {
     testImplementation(kotlin("test-js"))
+    implementation(kotlin("stdlib-js"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
     implementation("org.jetbrains:kotlin-react:16.13.1-pre.113-kotlin-1.4.0")
     implementation("org.jetbrains:kotlin-react-dom:16.13.1-pre.113-kotlin-1.4.0")
     implementation("org.jetbrains:kotlin-styled:1.0.0-pre.113-kotlin-1.4.0")
+    implementation(npm("axios", "^0.21.1", generateExternals = true))
+    implementation("com.github.samgarasx:kotlin-antd:4.8.6-pre.7-kotlin-1.4.30")
+    implementation(npm("antd", "^4.14.0"))
 }
 
 kotlin {
